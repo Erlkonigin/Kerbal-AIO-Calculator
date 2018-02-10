1ad340a457410a6732b961336d46177ff6ac6973
Erl_ <romanpopesku11@gmail.com>
2018-02-10 16:18:31 +0200

    Continuation

---
+++ DataLayer/ViewModels/BodySystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Models;
+
+namespace DataLayer.ViewModels
+{
+    public class BodySystem
+    {
+        public Planet CentralBody { get; set; }
+        public List<Planet> OrbitingBodys { get; set; }
+    }
+}
